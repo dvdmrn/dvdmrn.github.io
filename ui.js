ba1fdf531546f167140a6403b2d4efc5343ec4a0
dvdmrn <dgm.marino@gmail.com>
2020-05-03 01:51:26 -0400

    face

--- ui.js
+++ ui.js
@@ -1,13 +1,15 @@
-
+var lastWindow = "about"
 
   $( function() {
 
   	$("#projects").toggle();
+  	$("#contact").toggle();
 
-  	function openWindow(win){
-  		if(win=="about") $( "#about" ).toggle("size", 200 );
-  		if(win=="about") $( "#projects" ).toggle("size", 200 );
-
+  	function openWindow(win, last){
+  		$("#"+last).toggle("size", 200 );
+  		if(win=="about") {$( "#about" ).toggle("size", 200 );}
+  		if(win=="projects") $( "#projects" ).toggle("size", 200 );
+  		if(win=="contact") $( "#contact" ).toggle("size", 200 );
   	}
 
 
@@ -40,9 +42,20 @@
 
   	$("#about_selector").click(()=>{
   		console.log("clicked");
-  		openWindow("about");
+  		openWindow("about",lastWindow);
+  		lastWindow = "about";
   	})
 
-  
+  	$("#projects_selector").click(()=>{
+  		console.log("clicked");
+  		openWindow("projects",lastWindow);
+  		lastWindow = "projects";
+  	})
+
+  	$("#contact_selector").click(()=>{
+  		console.log("clicked");
+  		openWindow("contact",lastWindow);
+  		lastWindow = "contact";
+  	})  
 
   } );
