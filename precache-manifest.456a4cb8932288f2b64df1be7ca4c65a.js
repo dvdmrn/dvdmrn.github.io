cb94791111877ff30f6fb4db21306db49ac53422
dvdmrn <dgm.marino@gmail.com>
2020-08-05 01:29:47 -0700

    working public website

---
+++ precache-manifest.456a4cb8932288f2b64df1be7ca4c65a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6f539712a2e489393e22c159705949",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7402b704fb9afa7c0f5",
+    "url": "./static/css/main.bfd6a75d.chunk.css"
+  },
+  {
+    "revision": "f7b1c34597c496a08b69",
+    "url": "./static/js/10.5586d4a0.chunk.js"
+  },
+  {
+    "revision": "4de989218bbee566a5f8",
+    "url": "./static/js/11.df9bfc4c.chunk.js"
+  },
+  {
+    "revision": "e632025013df17f0ea13",
+    "url": "./static/js/12.df99316e.chunk.js"
+  },
+  {
+    "revision": "5089324710a0fe10f253",
+    "url": "./static/js/13.22b11c29.chunk.js"
+  },
+  {
+    "revision": "9abdfed23e7a44f47d29",
+    "url": "./static/js/14.650b4d33.chunk.js"
+  },
+  {
+    "revision": "0278b3b05f77245aa75f",
+    "url": "./static/js/2.dde14008.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "./static/js/2.dde14008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983478f7c927a0720103",
+    "url": "./static/js/3.cb8dd66b.chunk.js"
+  },
+  {
+    "revision": "fe0ddadd82d0151cd8b1",
+    "url": "./static/js/4.dccda5df.chunk.js"
+  },
+  {
+    "revision": "2d8d15759030e78406a2",
+    "url": "./static/js/5.1c1b7db5.chunk.js"
+  },
+  {
+    "revision": "8fafcf43e08460d5b1b7",
+    "url": "./static/js/6.c132f712.chunk.js"
+  },
+  {
+    "revision": "dca39ac2333fc7918b54",
+    "url": "./static/js/7.b901476c.chunk.js"
+  },
+  {
+    "revision": "50ca9f934a4bfc5805c0",
+    "url": "./static/js/8.a32e681f.chunk.js"
+  },
+  {
+    "revision": "32a1136d207f01c02fc6",
+    "url": "./static/js/9.8c6de530.chunk.js"
+  },
+  {
+    "revision": "e7402b704fb9afa7c0f5",
+    "url": "./static/js/main.a421b7d3.chunk.js"
+  },
+  {
+    "revision": "3b26790d6b10a8c79fa3",
+    "url": "./static/js/runtime-main.86f235dc.js"
+  },
+  {
+    "revision": "fc6e52ff44945f02d8927e1080b1e9ca",
+    "url": "./static/media/CoopRideHailing.fc6e52ff.png"
+  },
+  {
+    "revision": "706d1e237136bd3819f262e550cd8e51",
+    "url": "./static/media/algorithm-sketch.706d1e23.png"
+  },
+  {
+    "revision": "df6019e9cca3ca4de0ede891890fcc8d",
+    "url": "./static/media/app-UI.df6019e9.png"
+  },
+  {
+    "revision": "07521143f7f4c98d337472eed8ae31b8",
+    "url": "./static/media/avi-pink.07521143.png"
+  },
+  {
+    "revision": "c9474b7ad88ed2b2c3c15937126b958a",
+    "url": "./static/media/catgirl.c9474b7a.jpg"
+  },
+  {
+    "revision": "08941740bf00b0ee088a0d2bf47d8da7",
+    "url": "./static/media/construction_banner.08941740.gif"
+  },
+  {
+    "revision": "33298abebc56a47320cae16aa003e658",
+    "url": "./static/media/corpus-4.33298abe.gif"
+  },
+  {
+    "revision": "8be7b30855665ece5415b4f34755a3b7",
+    "url": "./static/media/corpus.8be7b308.png"
+  },
+  {
+    "revision": "e4e8abbb86d05866e5a195706f7d6b1f",
+    "url": "./static/media/projicon.e4e8abbb.png"
+  },
+  {
+    "revision": "74f7efa5cb41d386ac3d68112c6c096e",
+    "url": "./static/media/research-banner.74f7efa5.jpg"
+  },
+  {
+    "revision": "1ceddf2e120116bed737483db01fb59e",
+    "url": "./static/media/under_construction.1ceddf2e.gif"
+  },
+  {
+    "revision": "e00ffd4365f44ee1ff18b4e794c81cc9",
+    "url": "./static/media/vocalenhancement.e00ffd43.png"
+  },
+  {
+    "revision": "59f4834d152f6dc25c3149b559f1c001",
+    "url": "./static/media/voodleIcon.59f4834d.png"
+  },
+  {
+    "revision": "20340d698427a98f84ffd8b9566f266f",
+    "url": "./static/media/wehopingyoubuy.20340d69.jpg"
+  },
+  {
+    "revision": "834e593d604c3ee85384ce69527f92ac",
+    "url": "./static/media/whatshapicon.834e593d.png"
+  }
+]);
